b53d6d972a5a651af92ecbddfb44631a848f5a30
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2021-03-22 15:07:32 -0400

    assigned an init function

---
+++ lsystem.py
@@ -0,0 +1,14 @@
+# Caroline Ninganga
+# Project 7 L-Systems 
+# Version 1
+
+# An L-system requires two pieces of information:
+
+# the base string, and
+# the list of rules
+
+def __init__():
+    l_system = ['',[]]
+    return l_system
+
+def 
